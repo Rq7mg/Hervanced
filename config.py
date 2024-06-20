0d821b1576e20143782e66913ebf50cfc297aa8d
Rq7mg <163336518+Rq7mg@users.noreply.github.com>
2024-06-20 04:23:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "6058282484:AAG94Lm54HzldNtFRTQxCMi-KUv-4b7IYgU")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
